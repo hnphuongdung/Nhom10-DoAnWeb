734cb5f3844703a1fc240cf4bb8180f6c0daa91f
Phương Dung <67100160+hnphuongdung@users.noreply.github.com>
2020-07-04 19:16:46 +0700

    admin authlogin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,13 +11,23 @@ session_start();
 
 class AdminController extends Controller
 {
+    public function AuthLogin(){
+        $admin_id = Session::get('admin_id');
+        if($admin_id){
+            return Redirect::to('dashboard');
+        }else{
+            return Redirect::to('admin')->send();
+        }
+    }
     public function index(){
     	return view('admin_login');
     }
     public function show_dashboard(){
+        $this->AuthLogin();
     	return view('admin.dashboard');
     }
     public function dashboard(Request $request){
+        $this->AuthLogin();
     	$admin_email = $request->admin_email;
     	$admin_password = md5($request->admin_password);
 
@@ -32,6 +42,8 @@ class AdminController extends Controller
     	}
     }
     public function logout(){
+        $this->AuthLogin();
+
     	Session::put('admin_name',null);
     	Session::put('admin_id',null);
     	return Redirect::to('/admin');
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,17 +11,28 @@ session_start();
 
 class ProductController extends Controller
 {
+    public function AuthLogin(){
+        $admin_id = Session::get('admin_id');
+        if($admin_id){
+            return Redirect::to('dashboard');
+        }else{
+            return Redirect::to('admin')->send();
+        }
+    }
     public function add_product(){
+        $this->AuthLogin();
     	$cate_product = DB::table('tbl_category_product')->orderby('category_id','desc')->get();
     	return view('admin.add_product')->with('cate_product',$cate_product);
     }
     public function all_product(){
+        $this->AuthLogin();
     	$all_product = DB::table('tbl_product')->join('tbl_category_product','tbl_category_product.category_id','=','tbl_product.category_id')->orderby('tbl_product.product_id')->get();
     	$manager_product = view('admin.all_product')->with('all_product',$all_product);
     	return view('admin_layout')->with('admin.all_product',$manager_product);
 
     }
     public function save_product(Request $request){
+        $this->AuthLogin();
     	$data = array();
     	$data['product_name']=$request->product_name;
     	$data['product_desc']=$request->product_desc;
@@ -50,22 +61,26 @@ class ProductController extends Controller
     	return Redirect::to('add-product');
     }
     public function active_product($product_id){
+        $this->AuthLogin();
     	DB::table('tbl_product')->where('product_id',$product_id)->update(['product_status'=>1]);
     	Session::put('message','Kích hoạt danh mục sản phẩm thành công');
     	return Redirect::to('all-product');
     }
     public function unactive_product($product_id){
+        $this->AuthLogin();
     	DB::table('tbl_product')->where('product_id',$product_id)->update(['product_status'=>0]);
     	Session::put('message','Hủy kích hoạt danh mục sản phẩm thành công');
     	return Redirect::to('all-product');
     }
     public function edit_product($product_id){
+        $this->AuthLogin();
     	$cate_product = DB::table('tbl_category_product')->orderby('category_id','desc')->get();
     	$edit_product = DB::table('tbl_product')->where('product_id',$product_id)->get();
     	$manager_product = view('admin.edit_product')->with('edit_product',$edit_product)->with('cate_product',$cate_product);
     	return view('admin_layout')->with('admin.edit_product',$manager_product);
     }
     public function update_product(Request $request,$product_id){
+        $this->AuthLogin();
     	$data = array();
     	$data['product_name']=$request->product_name;
     	$data['product_price']=$request->product_price;
@@ -92,6 +107,7 @@ class ProductController extends Controller
 
     }
     public function delete_product($product_id){
+        $this->AuthLogin();
     	DB::table('tbl_product')->where('product_id',$product_id)->delete();
     	Session::put('message','Xóa sản phẩm thành công');
     	return Redirect::to('all-product');
